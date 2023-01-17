e186c3fc466b9fe8a47c690788b94ac2a698e684
Andriy Panasiuk <panasiukmail@gmail.com>
2023-01-17 11:47:12 +0200

    feat: structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
