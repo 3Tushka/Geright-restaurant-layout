bd4a3c06c4d8f07254b7c07abf29d054d2a3f08c
Andriy Panasiuk <panasiukmail@gmail.com>
2023-01-17 19:41:12 +0200

    feat: header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+import welcome from '../../assets/welcome.png';
 
-import './Header.css';
+import './Header.scss';
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+  <div className="header section__padding">
+    <div className="header__wrapper">
+      <SubHeading title="Chase The New Flavour" />
+      <h1 className="header__main-text">
+        The Key To Fine Dining
+      </h1>
+      <h3 className="header__sub-text">
+        Sit Tellus Lobortis Sed Senectus Vivamus Molestie. Condimentum Volutpat Morbi Facilisis Quam Scelerisque Sapien. Et, Penatibus Aliquam Amet Tellus
+      </h3>
+      <button className="header__menu-btn custom__button">Explore Menu</button>
+    </div>
+    <div className="header__image">
+      <img src={welcome} alt="welcome-photo" />
+    </div>
+  </div >
 );
 
 export default Header;
