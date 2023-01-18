f7bc8981b84d47fa94e4f260b21dc13c523f7aa6
Andriy Panasiuk <panasiukmail@gmail.com>
2023-01-18 15:57:06 +0200

    feat: about

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import knife from '../../assets/knife.png';
+import G from '../../assets/G.png';
+import { SubHeading } from '../../components';
 
-import './AboutUs.css';
+import './AboutUs.scss';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className="about" id='about'>
+    <div className="about__g-image flex__center">
+      <img src={G} alt="g-letter" />
+    </div>
+    <div className="about__content flex__center">
+      <div className="about__info">
+        <div className="about__info-title">
+          <h2>About Us</h2>
+          <SubHeading />
+        </div>
+        <div className="about__info-desc">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus provident soluta inventore debitis fugit saepe suscipit, a impedit exercitationem vitae, earum ut quia alias ab, veniam cumque. Ut, eveniet nemo?</p>
+        </div>
+        <div className="about__info-btn custom__button">
+          <p>Know More</p>
+        </div>
+      </div>
+      <div className="about__knife-image">
+        <img src={knife} alt="knife" />
+      </div>
+      <div className="about__history">
+        <div className="about__history-title">
+          <h2>Our History</h2>
+          <SubHeading />
+        </div>
+        <div className="about__history-desc">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus provident soluta inventore debitis fugit saepe suscipit, a impedit exercitationem vitae, earum ut quia alias ab, veniam cumque. Ut, eveniet nemo?</p>
+        </div>
+        <div className="about__history-btn custom__button">
+          <p>Know More</p>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
